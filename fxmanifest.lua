cba6ffdb663b65692af0cd3f8277fcccab4ed70d
Guudz <Kilian1702@outlook.com>
2023-01-29 05:56:28 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+
+author "Poseidon Leaks"
+description "Rose-V HUD"
+
+game "gta5"
+version "1.0"
+
+ui_page "html/html.html"
+
+files {
+	"html/html.html",
+	"html/*.js"
+}
+
+client_scripts {
+	"client/client.lua",
+}
